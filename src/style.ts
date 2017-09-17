3b0e2d02b658917a19f3d0ba38456e9118a8212b
Lukas Oppermann <lukas@vea.re>
2017-09-17 10:52:56 +0200

    style with columns according to size="breakpoint"

--- src/style.ts
+++ src/style.ts
@@ -13,30 +13,30 @@ export let style = `
 :host([autoflow=column]){
   grid-auto-flow: column dense;
 }
-::slotted([column~="1"]){ grid-column-end: span 1; }
-::slotted([column~="2"]){ grid-column-end: span 2; }
-::slotted([column~="3"]){ grid-column-end: span 3; }
-::slotted([column~="4"]){ grid-column-end: span 4; }
-::slotted([column~="5"]){ grid-column-end: span 5; }
-::slotted([column~="6"]){ grid-column-end: span 6; }
-::slotted([column~="7"]){ grid-column-end: span 7; }
-::slotted([column~="8"]){ grid-column-end: span 8; }
-::slotted([column~="9"]){ grid-column-end: span 9; }
-::slotted([column~="10"]){ grid-column-end: span 10; }
-::slotted([column~="11"]){ grid-column-end: span 11; }
-::slotted([column~="12"]){ grid-column-end: span 12; }
-::slotted([column~="13"]){ grid-column-end: span 13; }
-::slotted([column~="14"]){ grid-column-end: span 14; }
-::slotted([column~="15"]){ grid-column-end: span 15; }
-::slotted([column~="16"]){ grid-column-end: span 16; }
-::slotted([column~="17"]){ grid-column-end: span 17; }
-::slotted([column~="18"]){ grid-column-end: span 18; }
-::slotted([column~="19"]){ grid-column-end: span 19; }
-::slotted([column~="20"]){ grid-column-end: span 20; }
-::slotted([column~="21"]){ grid-column-end: span 21; }
-::slotted([column~="22"]){ grid-column-end: span 22; }
-::slotted([column~="23"]){ grid-column-end: span 23; }
-::slotted([column~="24"]){ grid-column-end: span 24; }
+::slotted([column~="1"]), :host([size="xs"]) ::slotted([column~="1xs"]), :host([size="s"]) ::slotted([column~="1s"]), :host([size="m"]) ::slotted([column~="1m"]), :host([size="l"]) ::slotted([column~="1l"]), :host([size="xl"]) ::slotted([column~="1xl"]), :host([size="xxl"]) ::slotted([column~="1xxl"]){ grid-column-end: span 1; }
+::slotted([column~="2"]), :host([size="xs"]) ::slotted([column~="2xs"]), :host([size="s"]) ::slotted([column~="2s"]), :host([size="m"]) ::slotted([column~="2m"]), :host([size="l"]) ::slotted([column~="2l"]), :host([size="xl"]) ::slotted([column~="2xl"]), :host([size="xxl"]) ::slotted([column~="2xxl"]){ grid-column-end: span 2; }
+::slotted([column~="3"]), :host([size="xs"]) ::slotted([column~="3xs"]), :host([size="s"]) ::slotted([column~="3s"]), :host([size="m"]) ::slotted([column~="3m"]), :host([size="l"]) ::slotted([column~="3l"]), :host([size="xl"]) ::slotted([column~="3xl"]), :host([size="xxl"]) ::slotted([column~="3xxl"]){ grid-column-end: span 3; }
+::slotted([column~="4"]), :host([size="xs"]) ::slotted([column~="4xs"]), :host([size="s"]) ::slotted([column~="4s"]), :host([size="m"]) ::slotted([column~="4m"]), :host([size="l"]) ::slotted([column~="4l"]), :host([size="xl"]) ::slotted([column~="4xl"]), :host([size="xxl"]) ::slotted([column~="4xxl"]){ grid-column-end: span 4; }
+::slotted([column~="5"]), :host([size="xs"]) ::slotted([column~="5xs"]), :host([size="s"]) ::slotted([column~="5s"]), :host([size="m"]) ::slotted([column~="5m"]), :host([size="l"]) ::slotted([column~="5l"]), :host([size="xl"]) ::slotted([column~="5xl"]), :host([size="xxl"]) ::slotted([column~="5xxl"]){ grid-column-end: span 5; }
+::slotted([column~="6"]), :host([size="xs"]) ::slotted([column~="6xs"]), :host([size="s"]) ::slotted([column~="6s"]), :host([size="m"]) ::slotted([column~="6m"]), :host([size="l"]) ::slotted([column~="6l"]), :host([size="xl"]) ::slotted([column~="6xl"]), :host([size="xxl"]) ::slotted([column~="6xxl"]){ grid-column-end: span 6; }
+::slotted([column~="7"]), :host([size="xs"]) ::slotted([column~="7xs"]), :host([size="s"]) ::slotted([column~="7s"]), :host([size="m"]) ::slotted([column~="7m"]), :host([size="l"]) ::slotted([column~="7l"]), :host([size="xl"]) ::slotted([column~="7xl"]), :host([size="xxl"]) ::slotted([column~="7xxl"]){ grid-column-end: span 7; }
+::slotted([column~="8"]), :host([size="xs"]) ::slotted([column~="8xs"]), :host([size="s"]) ::slotted([column~="8s"]), :host([size="m"]) ::slotted([column~="8m"]), :host([size="l"]) ::slotted([column~="8l"]), :host([size="xl"]) ::slotted([column~="8xl"]), :host([size="xxl"]) ::slotted([column~="8xxl"]){ grid-column-end: span 8; }
+::slotted([column~="9"]), :host([size="xs"]) ::slotted([column~="9xs"]), :host([size="s"]) ::slotted([column~="9s"]), :host([size="m"]) ::slotted([column~="9m"]), :host([size="l"]) ::slotted([column~="9l"]), :host([size="xl"]) ::slotted([column~="9xl"]), :host([size="xxl"]) ::slotted([column~="9xxl"]){ grid-column-end: span 9; }
+::slotted([column~="10"]), :host([size="xs"]) ::slotted([column~="10xs"]), :host([size="s"]) ::slotted([column~="10s"]), :host([size="m"]) ::slotted([column~="10m"]), :host([size="l"]) ::slotted([column~="10l"]), :host([size="xl"]) ::slotted([column~="10xl"]), :host([size="xxl"]) ::slotted([column~="10xxl"]){ grid-column-end: span 10; }
+::slotted([column~="11"]), :host([size="xs"]) ::slotted([column~="11xs"]), :host([size="s"]) ::slotted([column~="11s"]), :host([size="m"]) ::slotted([column~="11m"]), :host([size="l"]) ::slotted([column~="11l"]), :host([size="xl"]) ::slotted([column~="11xl"]), :host([size="xxl"]) ::slotted([column~="11xxl"]){ grid-column-end: span 11; }
+::slotted([column~="12"]), :host([size="xs"]) ::slotted([column~="12xs"]), :host([size="s"]) ::slotted([column~="12s"]), :host([size="m"]) ::slotted([column~="12m"]), :host([size="l"]) ::slotted([column~="12l"]), :host([size="xl"]) ::slotted([column~="12xl"]), :host([size="xxl"]) ::slotted([column~="12xxl"]){ grid-column-end: span 12; }
+::slotted([column~="13"]), :host([size="xs"]) ::slotted([column~="13xs"]), :host([size="s"]) ::slotted([column~="13s"]), :host([size="m"]) ::slotted([column~="13m"]), :host([size="l"]) ::slotted([column~="13l"]), :host([size="xl"]) ::slotted([column~="13xl"]), :host([size="xxl"]) ::slotted([column~="13xxl"]){ grid-column-end: span 13; }
+::slotted([column~="14"]), :host([size="xs"]) ::slotted([column~="14xs"]), :host([size="s"]) ::slotted([column~="14s"]), :host([size="m"]) ::slotted([column~="14m"]), :host([size="l"]) ::slotted([column~="14l"]), :host([size="xl"]) ::slotted([column~="14xl"]), :host([size="xxl"]) ::slotted([column~="14xxl"]){ grid-column-end: span 14; }
+::slotted([column~="15"]), :host([size="xs"]) ::slotted([column~="15xs"]), :host([size="s"]) ::slotted([column~="15s"]), :host([size="m"]) ::slotted([column~="15m"]), :host([size="l"]) ::slotted([column~="15l"]), :host([size="xl"]) ::slotted([column~="15xl"]), :host([size="xxl"]) ::slotted([column~="15xxl"]){ grid-column-end: span 15; }
+::slotted([column~="16"]), :host([size="xs"]) ::slotted([column~="16xs"]), :host([size="s"]) ::slotted([column~="16s"]), :host([size="m"]) ::slotted([column~="16m"]), :host([size="l"]) ::slotted([column~="16l"]), :host([size="xl"]) ::slotted([column~="16xl"]), :host([size="xxl"]) ::slotted([column~="16xxl"]){ grid-column-end: span 16; }
+::slotted([column~="17"]), :host([size="xs"]) ::slotted([column~="17xs"]), :host([size="s"]) ::slotted([column~="17s"]), :host([size="m"]) ::slotted([column~="17m"]), :host([size="l"]) ::slotted([column~="17l"]), :host([size="xl"]) ::slotted([column~="17xl"]), :host([size="xxl"]) ::slotted([column~="17xxl"]){ grid-column-end: span 17; }
+::slotted([column~="18"]), :host([size="xs"]) ::slotted([column~="18xs"]), :host([size="s"]) ::slotted([column~="18s"]), :host([size="m"]) ::slotted([column~="18m"]), :host([size="l"]) ::slotted([column~="18l"]), :host([size="xl"]) ::slotted([column~="18xl"]), :host([size="xxl"]) ::slotted([column~="18xxl"]){ grid-column-end: span 18; }
+::slotted([column~="19"]), :host([size="xs"]) ::slotted([column~="19xs"]), :host([size="s"]) ::slotted([column~="19s"]), :host([size="m"]) ::slotted([column~="19m"]), :host([size="l"]) ::slotted([column~="19l"]), :host([size="xl"]) ::slotted([column~="19xl"]), :host([size="xxl"]) ::slotted([column~="19xxl"]){ grid-column-end: span 19; }
+::slotted([column~="20"]), :host([size="xs"]) ::slotted([column~="20xs"]), :host([size="s"]) ::slotted([column~="20s"]), :host([size="m"]) ::slotted([column~="20m"]), :host([size="l"]) ::slotted([column~="20l"]), :host([size="xl"]) ::slotted([column~="20xl"]), :host([size="xxl"]) ::slotted([column~="20xxl"]){ grid-column-end: span 20; }
+::slotted([column~="21"]), :host([size="xs"]) ::slotted([column~="21xs"]), :host([size="s"]) ::slotted([column~="21s"]), :host([size="m"]) ::slotted([column~="21m"]), :host([size="l"]) ::slotted([column~="21l"]), :host([size="xl"]) ::slotted([column~="21xl"]), :host([size="xxl"]) ::slotted([column~="21xxl"]){ grid-column-end: span 21; }
+::slotted([column~="22"]), :host([size="xs"]) ::slotted([column~="22xs"]), :host([size="s"]) ::slotted([column~="22s"]), :host([size="m"]) ::slotted([column~="22m"]), :host([size="l"]) ::slotted([column~="22l"]), :host([size="xl"]) ::slotted([column~="22xl"]), :host([size="xxl"]) ::slotted([column~="22xxl"]){ grid-column-end: span 22; }
+::slotted([column~="23"]), :host([size="xs"]) ::slotted([column~="23xs"]), :host([size="s"]) ::slotted([column~="23s"]), :host([size="m"]) ::slotted([column~="23m"]), :host([size="l"]) ::slotted([column~="23l"]), :host([size="xl"]) ::slotted([column~="23xl"]), :host([size="xxl"]) ::slotted([column~="23xxl"]){ grid-column-end: span 23; }
+::slotted([column~="24"]), :host([size="xs"]) ::slotted([column~="24xs"]), :host([size="s"]) ::slotted([column~="24s"]), :host([size="m"]) ::slotted([column~="24m"]), :host([size="l"]) ::slotted([column~="24l"]), :host([size="xl"]) ::slotted([column~="24xl"]), :host([size="xxl"]) ::slotted([column~="24xxl"]){ grid-column-end: span 24; }
 
 ::slotted([row~="1"]){ grid-row-end: span 1; }
 ::slotted([row~="2"]){ grid-row-end: span 2; }
